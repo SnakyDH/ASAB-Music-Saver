597170d48bf44aca8519459104e7c517b53ccada
SnakyDH <skdanielg5@gmail.com>
2024-03-22 10:11:43 -0500

    feat: add navigation

--- app/src/main/java/com/snakydh/asab_music_saver/MainActivity.kt
+++ app/src/main/java/com/snakydh/asab_music_saver/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.snakydh.asab_music_saver.screens.HomeScreen
+import com.snakydh.asab_music_saver.navigation.AppNavigation
 import com.snakydh.asab_music_saver.ui.theme.ASABMusicSaverTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    HomeScreen()
+                    AppNavigation()
                 }
             }
         }
