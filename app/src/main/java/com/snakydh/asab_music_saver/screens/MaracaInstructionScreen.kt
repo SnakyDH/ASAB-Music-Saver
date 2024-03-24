467e735ce8664325c6de6563e5f67b8867173687
PaulaSanchez <pausanchezt12@gmail.com>
2024-03-23 20:39:29 -0500

    added buttons

--- app/src/main/java/com/snakydh/asab_music_saver/screens/MaracaInstructionScreen.kt
+++ app/src/main/java/com/snakydh/asab_music_saver/screens/MaracaInstructionScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -14,8 +15,10 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -29,6 +32,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.snakydh.asab_music_saver.navigation.AppScreens
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -80,7 +84,21 @@ fun MaracaInstructionScreen(navController: NavController) {
                 elevation = CardDefaults.cardElevation(10.dp)
             ) {
             }
+            Column(
+                modifier = Modifier
+                    .padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                FilledTonalButton(onClick = { navController.navigate(AppScreens.HomeScreen.route) }) {
+                    Text("¡Comenzar!", fontSize = 24.sp)
+                }
+                Spacer(modifier = Modifier.height(15.dp))
 
+                OutlinedButton(onClick = { navController.navigate(AppScreens.HomeScreen.route) }) {
+                    Text(text = "Atrás", fontSize = 24.sp)
+                }
+            }
         }
     }
 }
